741df7f654e24360923e5de84b2b529ef201c977
Ian Cavalheri Leite <ianmon.10@hotmail.com>
2021-04-19 16:17:11 -0300

    Finalização do primeiro projeto de DMO: HelloDroid

---
+++ hellodroid/app/src/main/java/br/edu/iancl/hellodroid/MainActivity.java
@@ -0,0 +1,20 @@
+package br.edu.iancl.hellodroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mensagemTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mensagemTextView = findViewById(R.id.textview_mensagem);
+
+        mensagemTextView.setText("Hello Droid");
+    }
+}
