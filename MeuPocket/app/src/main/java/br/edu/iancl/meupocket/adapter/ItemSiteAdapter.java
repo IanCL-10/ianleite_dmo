405ad89ed1319f0c6405932c687e9452cf28a540
Ian Cavalheri Leite <ianmon.10@hotmail.com>
2021-06-09 14:06:34 -0300

    Atualização com intent implícita

--- MeuPocket/app/src/main/java/br/edu/iancl/meupocket/adapter/ItemSiteAdapter.java
+++ MeuPocket/app/src/main/java/br/edu/iancl/meupocket/adapter/ItemSiteAdapter.java
@@ -1,6 +1,9 @@
 package br.edu.iancl.meupocket.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -64,6 +67,28 @@ public class ItemSiteAdapter extends RecyclerView.Adapter<ItemSiteAdapter.ViewHo
                 }
             }
         });
+
+        holder.openImageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(v == holder.openImageView){
+                    openClick(position);
+                }
+            }
+        });
+    }
+
+    private void openClick(int position){
+        Site site = mSites.get(position);
+
+        String url = site.getUrl().trim().replace(" ", "");
+        if(!url.startsWith("http://")){
+            url = "http://" + url;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        mContext.startActivity(intent);
     }
 
     public void setClickListener(RecyclerItemClickListener clickListener){
@@ -116,12 +141,14 @@ public class ItemSiteAdapter extends RecyclerView.Adapter<ItemSiteAdapter.ViewHo
         public TextView tituloTextView;
         public TextView enderecoTextView;
         public ImageView favoritoImageView;
+        public ImageView openImageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             tituloTextView = itemView.findViewById(R.id.text_titulo);
             enderecoTextView = itemView.findViewById(R.id.text_endereco);
             favoritoImageView = itemView.findViewById(R.id.img_icon_favorite);
+            openImageView = itemView.findViewById(R.id.img_icon_open);
 
             itemView.setOnClickListener(this);
         }
